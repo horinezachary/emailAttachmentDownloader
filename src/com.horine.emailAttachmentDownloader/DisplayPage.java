d40e0623581e6ce1f9e24f29904b5b9739142e4c
Zachary Horine <horine.zachary@gmail.com>
2018-12-10 22:26:01 -0800

    chooseFolder, chooseFile, update panel

--- src/com.horine.emailAttachmentDownloader/DisplayPage.java
+++ src/com.horine.emailAttachmentDownloader/DisplayPage.java
@@ -16,6 +16,13 @@ import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public class DisplayPage {
+
+    ArrayList<DisplayElem> elements;
+    JPanel elementPanel;
+    JFrame frame;
+    GlobalSettings settings;
+    EmailGetter getter;
+
     public DisplayPage(GlobalSettings settings, EmailGetter getter) {
         this.settings = settings;
         this.getter = getter;
@@ -30,13 +37,44 @@ public class DisplayPage {
         frame.add(elementPanel, BorderLayout.CENTER);
         frame.setVisible(true);
     }
+
     public void update(){
+        elementPanel = new JPanel();
+        for (int i = 0; i < elements.size(); i++){
+
+        }
+    }
+
     public void addElement(DisplayElem element){
+        elements.add(element);
+
     }
+
     public String chooseFolder(){
+        String filepath;
+
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fileChooser.showOpenDialog(frame);
+        File selectedfile = fileChooser.getSelectedFile();
+        filepath = selectedfile.getAbsolutePath();
+        return filepath;
     }
+
     public String chooseFile(String filename, String description, String extensions){
+        String filepath;
+
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setSelectedFile(new File(filename));
+        FileNameExtensionFilter filter = new FileNameExtensionFilter(description, extensions);
+        fileChooser.setFileFilter(filter);
+        fileChooser.showOpenDialog(frame);
+        File selectedfile = fileChooser.getSelectedFile();
+        filepath = selectedfile.getAbsolutePath();
+        return filepath;
     }
+
     private JMenuBar generateMenu(){
     }
     }
