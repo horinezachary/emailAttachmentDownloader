694d726e999fbf65dd429a7da16c6a181721e1e5
Zachary Horine <horine.zachary@gmail.com>
2018-12-16 23:48:32 -0800

    add initial constraints and setup for element

--- src/com.horine.emailAttachmentDownloader/DisplayElem.java
+++ src/com.horine.emailAttachmentDownloader/DisplayElem.java
@@ -1,7 +1,14 @@
 package com.horine.emailAttachmentDownloader;
 
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class DisplayElem extends JPanel{
 
@@ -11,18 +18,28 @@ public class DisplayElem extends JPanel{
     boolean toRemove;
     DisplayPage page;
 
-
     public DisplayElem(DisplayPage p, int id, String text){
+        super();
         this.page = p;
         this.id = id;
         this.text = text;
         onScreen = false;
         toRemove = false;
 
+        setMaximumSize(new Dimension(400,100));
+        setMinimumSize(new Dimension(397,50));
+
+        setLayout(new BorderLayout());
+        //setPreferredSize(new Dimension(397,50));
+        setBackground(Color.ORANGE);
+        setToolTipText(text);
+
+        addCloseButton();
     }
 
     private void close(){
         toRemove = true;
+        page.update();
     }
 
     public void setOnScreen(boolean onScreen) {
