7a691b11e381c93cf94e35f573c6ca5a80dffd8f
Zachary Horine <horine.zachary@gmail.com>
2018-12-18 16:04:31 -0800

    Add additional test elements

--- src/com.horine.emailAttachmentDownloader/Main.java
+++ src/com.horine.emailAttachmentDownloader/Main.java
@@ -14,7 +14,12 @@ public class Main {
         getter = new EmailGetter(settings.getPopHost(), settings.getStoreType(), settings.getAccount(), settings.getPassword(), imageSaver);
 
         displayPage = new DisplayPage(settings,getter);
-        displayPage.addElement(new DisplayElem(displayPage, 0,"THIS IS A TEST"));
+        displayPage.addElement(new DisplayElem(displayPage, 0,"THIS IS A TEST1"));
+        displayPage.addElement(new DisplayElem(displayPage, 1,"THIS IS A TEST2"));
+        displayPage.addElement(new DisplayElem(displayPage, 2,"THIS IS A TEST3"));
+        displayPage.addElement(new DisplayElem(displayPage, 3,"THIS IS A TEST4"));
+        displayPage.addElement(new DisplayElem(displayPage, 4,"THIS IS A TEST5"));
+
 
         if (settings.getSaveFolder() == null) {
             settings.setSaveFolder(displayPage.chooseFolder());
